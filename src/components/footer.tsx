5ade31f17660d2cf04e5c3874908f301ccadb8dc
RansomBroker <yadisoke@gmail.com>
2024-12-20 11:56:29 +0700

    fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="flex justify-center pb-3 text-white">
       <div className="mr-2 text-[1rem]">Powered by</div>
-      <Icons.logoType className="w-20" />
+      <Icons.logoType className="w-12" />
     </footer>
   );
 }
