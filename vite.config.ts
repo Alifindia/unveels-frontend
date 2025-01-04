1b250082d3e8e7c5e49861719c67ec32ed353b02
Muhammad Rizqi <muhammad-rizqi@live.com>
2025-01-04 07:11:51 +0700

    revert changes

--- vite.config.ts
+++ vite.config.ts
@@ -103,11 +103,6 @@ export default defineConfig({
         secure: true,
         rewrite: (path) => path.replace(/^\/rest/, "/en/rest"),
       },
-      '/tflite_web_api_cc_simd.js': {
-        target: 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tflite@0.0.1-alpha.9/dist/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/tflite_web_api_cc_simd\.js$/, 'tflite_web_api_cc_simd.js'),
-      },
     },
   },
   build: {
